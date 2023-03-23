908f4599ce7306d7384b10929b3cac10d36e16bb
shenqing <shenqin1@msu.edu>
2023-03-22 23:13:38 -0400

    Initial Commit

---
+++ Unsplash Searches/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Unsplash Searches
+//
+//  Created by 沈清昊 on 3/22/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Unsplash SearchesUITests/Unsplash_SearchesUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Unsplash_SearchesUITestsLaunchTests.swift
+//  Unsplash SearchesUITests
+//
+//  Created by 沈清昊 on 3/22/23.
+//
+
+import XCTest
+
+class Unsplash_SearchesUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
