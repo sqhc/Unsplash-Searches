397e8640519a196315dc55c65b11ba230ed5be57
shenqing <shenqin1@msu.edu>
2023-03-24 10:11:02 -0400

    Add more constant values.

--- Unsplash Searches/Globals.swift
+++ Unsplash Searches/Globals.swift
@@ -14,3 +14,8 @@ enum Request{
 }
 
 var searchCollectionsLink = "https://api.unsplash.com/search/collections?client_id=pyMs87cvZKpi4JmARh-uebHiGE14Ahepb89fQmJ0cvw"
+var searchPhotosLink = "https://api.unsplash.com/search/photos?client_id=pyMs87cvZKpi4JmARh-uebHiGE14Ahepb89fQmJ0cvw"
+var searchRandomPhotoLink = "https://api.unsplash.com/photos/random?client_id=pyMs87cvZKpi4JmARh-uebHiGE14Ahepb89fQmJ0cvw"
+var searchUsersLink = "https://api.unsplash.com/search/users?client_id=pyMs87cvZKpi4JmARh-uebHiGE14Ahepb89fQmJ0cvw"
+
+var client_id = "?client_id=pyMs87cvZKpi4JmARh-uebHiGE14Ahepb89fQmJ0cvw"
